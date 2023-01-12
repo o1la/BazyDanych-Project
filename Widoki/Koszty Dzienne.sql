afc6bacf14d34471fe3fe731f2fb2314112c0fad
aleksandra <ola.grygorczyk@gmail.com>
2023-01-12 16:28:42 +0100

    koszty dzienne

--- Widoki/Koszty Dzienne.sql
+++ Widoki/Koszty Dzienne.sql
@@ -1,5 +1,12 @@
---Create View [Statystyki Kosztow] AS
-Select * FROM 
+--Creat View [Koszty Dzienne] AS
+SELECT IIF(SumaWypos.DATA is NULL, SumaInne.DATA, SumaWypos.DATA) as Data, IIF(SumaWypos.[Suma Dzienna] is NULL, SumaInne.[Suma Dzienna], IIF(SumaInne.[Suma Dzienna] is NULL,SumaWypos.[Suma Dzienna], SumaWypos.[Suma Dzienna]+ SumaInne.[Suma Dzienna])) as [Suma Dzienna] FROM
 (
-    
-)
+    SELECT Data as Data, SUM([Cena jednostkowa]* [Ilość]) [Suma Dzienna] FROM [Zakupy Wyposażenia]
+    Group By (Data)
+) as SumaWypos
+FULL OUTER JOIN
+(
+    Select Data, SUM(Koszt) [Suma Dzienna] FROM [Inne Wydatki]
+    Group By (Data)
+) as SumaInne
+On SumaInne.Data = SumaWypos.Data
