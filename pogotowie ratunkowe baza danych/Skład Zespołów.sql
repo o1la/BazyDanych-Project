625657f3dc519f86835df23f0ea98b0060281d19
o1la <91129287+o1la@users.noreply.github.com>
2023-01-12 11:21:59 +0100

    view sklad zespolow

---
+++ pogotowie ratunkowe baza danych/Skład Zespołów.sql
@@ -0,0 +1,11 @@
+CREATE VIEW [Skład Zespołów] AS
+SELECT Z.Nazwa AS [Nazwa Zespołu], P.Imię AS [Imię], P.Nazwisko [Nazwisko], S.Nazwa [Stanowisko] 
+FROM Zespoły Z 
+    LEFT JOIN
+    Pracownicy P ON (Z.ID = P.[ID Zespołu])
+    JOIN 
+    Stanowiska S ON (P.[ID Stanowiska] = S.[ID Stanowiska]);
+
+--DROP VIEW [Skład Zespołów]
+
+SELECT * FROM [Skład Zespołów]
