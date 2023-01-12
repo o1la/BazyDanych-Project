374a7c79a2526b9500970012d4f2c7b2f236dbe2
o1la <91129287+o1la@users.noreply.github.com>
2023-01-12 12:15:15 +0100

    zgloszenia dzienne

---
+++ pogotowie ratunkowe baza danych/Zgłoszenia dzienne.sql
@@ -0,0 +1,10 @@
+CREATE VIEW [Zgłoszenia dzienne] AS
+    SELECT 
+    (SELECT COUNT(*) FROM Zgłoszenia) AS [Ilość Zgłoszeń],
+    (SELECT COUNT(*) FROM Wyjazdy) AS [Ilość Wyjazdów],
+    (SELECT CAST((
+        CAST((SELECT COUNT(*) FROM Wyjazdy) AS FLOAT) / CAST((SELECT COUNT(*) FROM Zgłoszenia) AS FLOAT) 
+       *100) AS DECIMAL(10,2) )) AS [Procent]
+
+-- DROP VIEW [Zgłoszenia dzienne]
+-- SELECT * FROM [Zgłoszenia dzienne]
