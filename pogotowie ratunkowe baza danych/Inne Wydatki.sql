e13f953a125e267cd04c5ce3281c8f8692c8bb1e
o1la <91129287+o1la@users.noreply.github.com>
2023-01-12 12:57:00 +0100

    folder i daty do wydatkow

--- pogotowie ratunkowe baza danych/Inne Wydatki.sql
+++ pogotowie ratunkowe baza danych/Inne Wydatki.sql
@@ -2,13 +2,14 @@ CREATE TABLE [Inne Wydatki]
 (
  [ID] INT,
  [Typ Wydatku] VARCHAR(50),
- [Koszt] MONEY
+ [Koszt] MONEY,
+ [Data] DATE
 )
  
 --SELECT * FROM [Inne Wydatki]
 --DROP TABLE [Inne Wydatki]
 
 INSERT INTO [Inne Wydatki] VALUES
-(1, 'Paliwo', 700.00),
-(2, 'Serwis sprzętu', 300.00),
-(3, 'Serwis samochodu', 1500.00);
+(1, 'Paliwo', 700.00, '2022-12-13'),
+(2, 'Serwis sprzętu', 300.00, '2022-12-18'),
+(3, 'Serwis samochodu', 1500.00, '2022-12-02');
--- pogotowie ratunkowe baza danych/Zakupy Wyposażenia.sql
+++ pogotowie ratunkowe baza danych/Zakupy Wyposażenia.sql
@@ -4,7 +4,8 @@ CREATE TABLE [Zakupy Wyposażenia]
  [ID Typu] INT,
  [ID Produktu] INT,
  [Ilość] INT,
- [Cena jednostkowa] MONEY
+ [Cena jednostkowa] MONEY,
+ [Data] DATE
     CONSTRAINT FK_IDtypzakupy FOREIGN KEY ([ID Typu])
     REFERENCES [Typ Wyposażenia] ([ID]),
     CONSTRAINT FK_IDprduktuZakup FOREIGN KEY ([ID Produktu])
@@ -15,8 +16,8 @@ CREATE TABLE [Zakupy Wyposażenia]
 --DROP TABLE [Zakupy Wyposażenia]
 
 INSERT INTO [Zakupy Wyposażenia] VALUES
-(1, 1, 5, 2, 702.53), 
-(2, 2, 7, 35, 10.90),
-(3, 2, 8, 5, 16.02),
-(4, 4, 11, 2, 119.99),
-(5, 6, 20, 20, 50.82);
+(1, 1, 5, 2, 702.53, '2022-12-05'), 
+(2, 2, 7, 35, 10.90, '2022-12-13'),
+(3, 2, 8, 5, 16.02, '2022-12-15'),
+(4, 4, 11, 2, 119.99, '2022-12-20'),
+(5, 6, 20, 20, 50.82, '2022-12-23');
