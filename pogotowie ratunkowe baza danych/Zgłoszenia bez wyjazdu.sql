12fb39d0cf411fe637c9329d203ec0cbf79913ba
o1la <91129287+o1la@users.noreply.github.com>
2023-01-12 12:45:14 +0100

    zgloszenia bez wyjazdow

---
+++ pogotowie ratunkowe baza danych/Zgłoszenia bez wyjazdu.sql
@@ -0,0 +1,9 @@
+CREATE VIEW [Zgłoszenia bez wyjazdu] AS
+SELECT [Zgłoszenia bez wyjazdu].[ID Zgłoszenia], Z.[Powód] FROM
+    (SELECT [ID Zgłoszenia] FROM Zgłoszenia WHERE [ID Zgłoszenia] NOT IN 
+    ( SELECT [ID Zgłoszenia] FROM Wyjazdy )) AS [Zgłoszenia bez wyjazdu]
+    LEFT JOIN 
+       Zgłoszenia Z ON (Z.[ID Zgłoszenia]=[Zgłoszenia bez wyjazdu].[ID Zgłoszenia]) 
+    
+-- SELECT * FROM [Zgłoszenia bez wyjazdu]
+-- DROP VIEW [Zgłoszenia bez wyjazdu]
