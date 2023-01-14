ed795d3f282a1c562a2469f45f26a2f7f95e1a74
aleksandra <ola.grygorczyk@gmail.com>
2023-01-14 18:27:26 +0100

    święta

---
+++ pogotowie ratunkowe baza danych/Święta.sql
@@ -0,0 +1,16 @@
+CREATE TABLE [Święta] (
+[ID] INT NOT NULL,
+[Nazwa] VARCHAR(50),
+[Data] DATE
+);
+
+INSERT INTO [Święta] VALUES 
+(1, 'Mikołajki', '2022-12-06'),
+(2, 'Wigilia', '2022-12-24'),
+(3, 'Boże Narodzenie', '2022-12-25'),
+(4, 'Drugi Dzień Świąt Bożego Narodzenia', '2022-12-26'),
+(5, 'Nowy Rok', '2023-01-01');
+
+
+--SELECT * FROM [Święta]
+--DROP TABLE [Święta]
