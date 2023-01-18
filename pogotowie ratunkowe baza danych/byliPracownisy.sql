78b473ae3ff0be25a024466a361d7642e71a328e
ignajlo <96392714+ignajlo@users.noreply.github.com>
2023-01-18 18:19:00 +0100

    Create byliPracownisy.sql

---
+++ pogotowie ratunkowe baza danych/byliPracownisy.sql
@@ -0,0 +1,11 @@
+CREATE TABLE [Byli_Pracownicy]
+(
+    [ID] INT PRIMARY KEY,
+ [Imię] VARCHAR(50),
+ [Nazwisko] VARCHAR(50),
+ [PESEL] VARCHAR(11),
+ [ID Stanowiska] INT NOT NULL,
+ [Miasto] VARCHAR(50),
+ [Ulica] VARCHAR(50),
+ [Data usunięcia] DATE
+)
