f53e596bf0bb0e710cb0a3586237f92ff193e610
aleksandra <ola.grygorczyk@gmail.com>
2023-01-21 18:31:53 +0100

    liczba przepracwoanyc h urlop

--- Triggery/Procedury/Liczba przepracowanych godzin.sql
+++ Triggery/Procedury/Liczba przepracowanych godzin.sql
@@ -7,13 +7,16 @@ AS
 begin 
     SELECT P.ID, P.Imię, P.Nazwisko, SUM(DATEDIFF(HOUR,GZ.[Clock in],GZ.[Clock out])) AS [Przepracowane godziny]
     FROM [Pracownicy] P
-        JOIN 
+        LEFT JOIN 
     Zespoły Z ON (P.[ID Zespołu] = Z.[ID])
         JOIN
     [Grafik Zespołów] GZ ON (GZ.[ID Zespołu] = Z.[ID])
-    WHERE @EmployeeID = P.[ID] AND @From < GZ.[Clock in] AND @To > GZ.[Clock out]
+    WHERE @EmployeeID = P.[ID] AND @From < GZ.[Clock in] AND @To > GZ.[Clock out] AND NOT EXISTS (
+        SELECT * FROM Urlopy U
+        WHERE @From <= U.[Początek] AND @To >= U.[Koniec] AND  @EmployeeID = U.[ID Pracownika] AND U.[Płatny] = 0
+    )
     GROUP BY P.ID, P.Imię, P.Nazwisko
 end
 
 
---exec [Liczba Przepracowanych Godzin] '2022-12-01', '2022-12-04', 3
+--exec [Liczba Przepracowanych Godzin] '2022-12-01', '2022-12-04', 4
