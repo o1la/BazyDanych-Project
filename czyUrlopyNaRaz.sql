257f4d70bdede7f4227abc59add05db04540c08f
ignajlo <96392714+ignajlo@users.noreply.github.com>
2023-01-21 16:17:04 +0100

    Update czyUrlopyNaRaz.sql

--- czyUrlopyNaRaz.sql
+++ czyUrlopyNaRaz.sql
@@ -7,13 +7,10 @@ BEGIN
 
     SELECT @overlap = COUNT(*)
     FROM Urlopy
-    WHERE EXISTS (SELECT 1 FROM Urlopy B, Urlopy A WHERE A.[ID Pracownika] <> B.[ID Pracownika] AND ((A.[Początek] <= B.[Koniec] AND A.[Początek] >= B.[Początek]) OR (A.[Koniec] <= B.[Koniec] AND A.[Koniec] >= B.[Początek])))
+    WHERE EXISTS (SELECT 1 FROM Urlopy B, Urlopy A, Pracownicy Pa, Pracownicy Pb WHERE A.[ID Pracownika] <> B.[ID Pracownika] AND A.[ID Pracownika] = Pa.ID AND B.[ID Pracownika] = Pb.ID AND Pb.[ID Zespołu]=Pa.[ID Zespołu] AND ((A.[Początek] <= B.[Koniec] AND A.[Początek] >= B.[Początek]) OR (A.[Koniec] <= B.[Koniec] AND A.[Koniec] >= B.[Początek])))
 
     IF @overlap > 0 
         SET @overlap = 1
 
     RETURN @overlap;
 END
-
---ALTER TABLE Urlopy
---ADD CONSTRAINT chk_overlap_vacation CHECK (dbo.check_overlap_vacation() = 0)
